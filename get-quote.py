76bcdbec09ef0f53c5832d2d51261027b8bcfcf7
Jason Zhao <famousdraw@163.com>
2020-04-17 20:24:20 +0800

    add write_quote.py revised last calculation in get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,17 +1,20 @@
 import random
 def primary():
-    last=13
-    rnd=random.randint(0,last)
     print("Keep it logically awesome.")
-
     f = open("quotes.txt")
     quotes = f.readlines()
+    last=len(quotes)
+    rnd=random.randint(0,last)
     #quotes=''.join(quotes)
+
     #quotes=quotes.strip('\n')
     f.close()
 
-
-    print(quotes[rnd])
+    for i in range(5):
+        if rnd+i>=len(quotes):
+            #print(rnd+i)
+            rnd-=len(quotes)+i     
+        print(quotes[rnd+i].strip())
 
 if __name__== "__main__":
   primary()
---
+++ write_quote.py
@@ -0,0 +1,3 @@
+f=open( "quotes.txt" ,'a')
+f.write(" Learn about file writing and add quotes programmatically\n")
+f.close()
